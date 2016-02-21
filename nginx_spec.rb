69b6956965557b5fc718db3b81935ffded689358
jameel-rean <jameel.mohammed@reancloud.com>
2016-02-21 21:48:51 +0530

    changed file

--- nginx_spec.rb
+++ nginx_spec.rb
@@ -15,5 +15,5 @@ end
 
 describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
-  it { should contain "worker_connections 1024;" }
+  it { should contain "worker_connections 768;" }
 end
